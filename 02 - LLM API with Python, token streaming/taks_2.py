a2bc68f51353df25cc94196ffd9e85edab1e2a0b
Islom Makhmudov <im.imakhmudov@gmail.com>
2025-01-13 12:29:57 +0500

    First commit

--- 02 - LLM API with Python, token streaming/taks_2.py
+++ 02 - LLM API with Python, token streaming/taks_2.py
@@ -1,12 +1,15 @@
 from openai import OpenAI
 client = OpenAI()
 
+with open("lesson-1-transcript.txt", "r") as file:
+  file_content = file.read()
+user_request = f"Write a blogpost the lecture based on this content:\n{file_content}"
 completion = client.chat.completions.create(
   model="gpt-4o-mini-2024-07-18",
   max_tokens=100,
   messages=[
     {"role": "developer", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "What is AI.!"}
+    {"role": "user", "content": file_content}
   ]
 )
 
